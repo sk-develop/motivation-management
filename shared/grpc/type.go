93f70ca0d6b1998ab2e3758573b458c6ff73395f
sk <v22w6y@gmail.com>
2021-12-09 20:36:42 +0900

    Remove unnecessary code from shared/grpc/type.go

--- shared/grpc/type.go
+++ shared/grpc/type.go
@@ -6,17 +6,7 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-type grpcType struct{}
-
-type GrpcType interface {
-	Timestamp(t time.Time) *timestamppb.Timestamp
-}
-
-func NewGrpcType() GrpcType {
-	return &grpcType{}
-}
-
-func (grpcType) Timestamp(t time.Time) *timestamppb.Timestamp {
+func Timestamp(t time.Time) *timestamppb.Timestamp {
 	return &timestamppb.Timestamp{
 		Seconds: t.Unix(),
 		Nanos:   int32(t.Nanosecond()),
