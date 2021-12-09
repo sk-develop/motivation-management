4dd68ad196db6547a54689e430257af9b8ba48d8
sk <v22w6y@gmail.com>
2021-12-09 20:46:20 +0900

    Move task model to domain/model/task

---
+++ services/task/internal/domain/model/task/task.go
@@ -0,0 +1,26 @@
+package task
+
+type Task struct {
+	ID        ID
+	UserID    UserID
+	Name      Name
+	Completed Completed
+	DueDate   DueDate
+	CreatedAt CreatedAt
+	UpdatedAt UpdatedAt
+}
+
+const (
+	lengthLimit = 255
+	minLength   = 1
+)
+
+type Tasks []Task
+
+func New(
+	userID UserID,
+	name Name,
+	dueDate DueDate,
+) *Task {
+	return &Task{UserID: userID, Name: name, DueDate: dueDate}
+}
