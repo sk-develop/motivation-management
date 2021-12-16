b5394c3ba296586134b2a4afa8d1a1c064e93c0c
sk <v22w6y@gmail.com>
2021-12-16 20:16:30 +0900

    Add task create interface to .proto

--- shared/proto/task/task_grpc.pb.go
+++ shared/proto/task/task_grpc.pb.go
@@ -18,8 +18,8 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type TaskServiceClient interface {
-	CreateTask(ctx context.Context, in *CreateTaskReq, opts ...grpc.CallOption) (*CreateTaskRes, error)
 	GetTasks(ctx context.Context, in *GetTasksReq, opts ...grpc.CallOption) (*GetTasksRes, error)
+	CreateTask(ctx context.Context, in *CreateTaskReq, opts ...grpc.CallOption) (*CreateTaskRes, error)
 	GetCompletedTasksTotal(ctx context.Context, in *GetTasksTotalReq, opts ...grpc.CallOption) (*GetCompletedTasksTotalRes, error)
 	GetCompletedTasksDailyTotal(ctx context.Context, in *GetTasksTotalReq, opts ...grpc.CallOption) (*CompletedTasksDailyTotalRes, error)
 }
@@ -32,18 +32,18 @@ func NewTaskServiceClient(cc grpc.ClientConnInterface) TaskServiceClient {
 	return &taskServiceClient{cc}
 }
 
-func (c *taskServiceClient) CreateTask(ctx context.Context, in *CreateTaskReq, opts ...grpc.CallOption) (*CreateTaskRes, error) {
-	out := new(CreateTaskRes)
-	err := c.cc.Invoke(ctx, "/task.taskService/CreateTask", in, out, opts...)
+func (c *taskServiceClient) GetTasks(ctx context.Context, in *GetTasksReq, opts ...grpc.CallOption) (*GetTasksRes, error) {
+	out := new(GetTasksRes)
+	err := c.cc.Invoke(ctx, "/task.taskService/GetTasks", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *taskServiceClient) GetTasks(ctx context.Context, in *GetTasksReq, opts ...grpc.CallOption) (*GetTasksRes, error) {
-	out := new(GetTasksRes)
-	err := c.cc.Invoke(ctx, "/task.taskService/GetTasks", in, out, opts...)
+func (c *taskServiceClient) CreateTask(ctx context.Context, in *CreateTaskReq, opts ...grpc.CallOption) (*CreateTaskRes, error) {
+	out := new(CreateTaskRes)
+	err := c.cc.Invoke(ctx, "/task.taskService/CreateTask", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -72,8 +72,8 @@ func (c *taskServiceClient) GetCompletedTasksDailyTotal(ctx context.Context, in 
 // All implementations must embed UnimplementedTaskServiceServer
 // for forward compatibility
 type TaskServiceServer interface {
-	CreateTask(context.Context, *CreateTaskReq) (*CreateTaskRes, error)
 	GetTasks(context.Context, *GetTasksReq) (*GetTasksRes, error)
+	CreateTask(context.Context, *CreateTaskReq) (*CreateTaskRes, error)
 	GetCompletedTasksTotal(context.Context, *GetTasksTotalReq) (*GetCompletedTasksTotalRes, error)
 	GetCompletedTasksDailyTotal(context.Context, *GetTasksTotalReq) (*CompletedTasksDailyTotalRes, error)
 	mustEmbedUnimplementedTaskServiceServer()
@@ -83,11 +83,11 @@ type TaskServiceServer interface {
 type UnimplementedTaskServiceServer struct {
 }
 
-func (UnimplementedTaskServiceServer) CreateTask(context.Context, *CreateTaskReq) (*CreateTaskRes, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method CreateTask not implemented")
-}
 func (UnimplementedTaskServiceServer) GetTasks(context.Context, *GetTasksReq) (*GetTasksRes, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetTasks not implemented")
+}
+func (UnimplementedTaskServiceServer) CreateTask(context.Context, *CreateTaskReq) (*CreateTaskRes, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreateTask not implemented")
 }
 func (UnimplementedTaskServiceServer) GetCompletedTasksTotal(context.Context, *GetTasksTotalReq) (*GetCompletedTasksTotalRes, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetCompletedTasksTotal not implemented")
@@ -108,24 +108,6 @@ func RegisterTaskServiceServer(s grpc.ServiceRegistrar, srv TaskServiceServer) {
 	s.RegisterService(&TaskService_ServiceDesc, srv)
 }
 
-func _TaskService_CreateTask_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(CreateTaskReq)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(TaskServiceServer).CreateTask(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/task.taskService/CreateTask",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(TaskServiceServer).CreateTask(ctx, req.(*CreateTaskReq))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
 func _TaskService_GetTasks_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(GetTasksReq)
 	if err := dec(in); err != nil {
@@ -140,6 +122,24 @@ func _TaskService_GetTasks_Handler(srv interface{}, ctx context.Context, dec fun
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(TaskServiceServer).GetTasks(ctx, req.(*GetTasksReq))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _TaskService_CreateTask_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CreateTaskReq)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(TaskServiceServer).CreateTask(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/task.taskService/CreateTask",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(TaskServiceServer).CreateTask(ctx, req.(*CreateTaskReq))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -188,12 +188,12 @@ var TaskService_ServiceDesc = grpc.ServiceDesc{
 	HandlerType: (*TaskServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "CreateTask",
-			Handler:    _TaskService_CreateTask_Handler,
-		},
-		{
 			MethodName: "GetTasks",
 			Handler:    _TaskService_GetTasks_Handler,
+		},
+		{
+			MethodName: "CreateTask",
+			Handler:    _TaskService_CreateTask_Handler,
 		},
 		{
 			MethodName: "GetCompletedTasksTotal",
